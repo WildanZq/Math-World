ee8a3f935e552ceacb16b576bebd5267e083a11a
Ahmad Dimas Abid M <ahmaddimas0@gmail.com>
2017-08-25 01:25:38 +0700

    add lock level

---
+++ Assets/Scripts/LocalDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Boomlagoon.JSON;
+
+public class LocalDatabase : MonoBehaviour {
+	public static LocalDatabase Instance { get; private set; }
+
+	private void Awake()
+	{
+		Instance = this;
+
+		if (!PlayerPrefs.HasKey("lastLevel"))
+		{
+			PlayerPrefs.SetInt("lastLevel", 1);
+		}
+	}
+
+	public void setLastLevel(int level) {
+		int l = PlayerPrefs.GetInt("lastLevel") + 1;
+		if (level == l) PlayerPrefs.SetInt("lastLevel", l);
+	}
+}
---
+++ Assets/Scripts/SetLastLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetLastLevel : MonoBehaviour {
+	[SerializeField]
+	private GameObject[] btnLevel;
+	public Sprite lockImage;
+
+	private void Start() {
+		for (int i = 0; i < btnLevel.Length; i++) {
+			if (PlayerPrefs.GetInt ("lastLevel") >= (i + 1)) {
+				btnLevel [i].GetComponent<Button> ().enabled = true;
+			} else {
+				btnLevel [i].GetComponent<Button> ().enabled = false;
+				btnLevel [i].GetComponent<Image>().sprite = lockImage;
+			}
+		}
+	}
+}
